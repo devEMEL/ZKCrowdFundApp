f38ba07640f44d42f0d088d3d2c28b56df4712b0
devEMEL <dominichenry882@gmail.com>
2025-03-29 15:13:28 +0100

    first commit

---
+++ zkCrowdFund/methods/guest/src/main.rs
@@ -0,0 +1,16 @@
+
+
+use risc0_zkvm::guest::env;
+
+const MIN_DONATION_AMOUNT_CONSTANT: u64 = 50_000_000_000_000_000; // 0.05 ETH in wei
+
+fn main() {
+    // Read the input
+    let min_donation_amount: u64 = env::read();
+    // Check if donation meets the minimum amount
+    let result = min_donation_amount >= MIN_DONATION_AMOUNT_CONSTANT;
+
+    // Write public output to the journal
+    env::commit(&result);
+}
+
