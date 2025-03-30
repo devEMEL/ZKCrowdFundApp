88841b8efdaef0b5722938bca224358a882f7d7e
devEMEL <dominichenry882@gmail.com>
2025-03-30 11:47:24 +0100

    changes v1

--- zkCrowdFund/methods/guest/src/main.rs
+++ zkCrowdFund/methods/guest/src/main.rs
@@ -5,8 +5,10 @@ use risc0_zkvm::guest::env;
 
 fn main() {
     // Read the input
-    let min_donation_amount = env::read::<i32>();
-    let min_donation_amount_constant = env::read::<i32>();
+
+    let min_donation_amount: u32 = env::read();
+    let min_donation_amount_constant: u32 = env::read();
+    
     // Check if donation meets the minimum amount
     let result = min_donation_amount >= min_donation_amount_constant;
 
