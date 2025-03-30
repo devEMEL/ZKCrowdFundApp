5eb656833f2cdeeea3cc1233d88ba339c88f6aab
devEMEL <dominichenry882@gmail.com>
2025-03-30 12:49:01 +0100

    updated guest code

--- zkCrowdFund/methods/guest/src/main.rs
+++ zkCrowdFund/methods/guest/src/main.rs
@@ -8,11 +8,13 @@ fn main() {
 
     let min_donation_amount: u32 = env::read();
     let min_donation_amount_constant: u32 = env::read();
-    
+
     // Check if donation meets the minimum amount
-    let result = min_donation_amount >= min_donation_amount_constant;
+    if min_donation_amount < min_donation_amount_constant {
+        panic!("Donation amount doesn't meet minimum amount")
+    }
 
     // Write public output to the journal
-    env::commit(&result);
+    env::commit(&min_donation_amount);
 }
 
